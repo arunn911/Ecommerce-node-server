6834116d87cc9e12545c66afaa98b73df1005fef
arunn911 <rajarunsathy@gmail.com>
2021-11-30 18:50:35 +0530

    product schema changed

--- models/Product.js
+++ models/Product.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
 {
-    title:{type:String, required:true, unique:true},
-    desc:{type:String, required:true,},
+    title:{type:String, required:true},
+    desc:{type:String, required:true},
     img:{type:String, required:true},
     categories:{type:Array},
-    size:{type:String,},
-    color:{type:String,},
+    size:{type:Array},
+    color:{type:String},
     price:{type:Number, required:true},
+    inStock:{type:Boolean, default:true}
     },
  {timestamps:true}
 );
